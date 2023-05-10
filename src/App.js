617fff5cacb1f3954c87c1312ee9a31a2bfccf5e
3ambob <montaserkanan4@gmail.com>
2023-05-10 03:21:50 +0300

    react task 2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './components/About/About';
+import Home from './components/Home'
+ 
+ export default function App() {
+   return (
+    <>
+    <About />
+    <Home />
+    </>
+   )
+ }
+ 
